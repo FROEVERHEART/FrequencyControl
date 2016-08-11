063d4cef4401f14adf12543dde5a62aa38c422e0
FROEVERHEART <360549968@QQ.COM>
2016-08-11 15:37:22 +0800

    Add files via upload

---
+++ src/main/resources/TestController.java
@@ -0,0 +1,41 @@
+/**
+ *  @Title: Test.java 
+ *  @Package com.cn21.FrequencyControl 
+ *  @Description: TODO(用一句话描述该文件做什么) 
+ *  @author chenxiaofeng
+ *  @date 2016年8月8日 下午5:21:52 
+ *  @version V1.0 
+ */
+package com.cn21.FrequencyControl.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.cn21.FrequencyControl.module.Example;
+import com.cn21.FrequencyControl.service.ExampleService;
+
+/**
+ * @author chenxiaofeng
+ * @date 2016年8月8日
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	@Autowired
+	private ExampleService exampleService;
+
+	/**
+	 * 欢迎页面
+	 * @return void
+	 */
+	@RequestMapping(value = "/welcome.do")
+	public ModelAndView welcome() {
+		Example example = exampleService.showExample("example");//获取例子
+		System.out.print(example.getName());
+		ModelAndView modelAndView = new ModelAndView("welcome");
+		modelAndView.addObject("example", example);
+		return modelAndView;
+	}
+}
